e77a5280b43c57a8fe4cc76c5f874726e9c1005a
Rory Mulligan <rmulligan@sitepen.com>
2017-03-22 10:35:27 -0400

    Adding test for Tree

--- tests/unit/DataExport.js
+++ tests/unit/DataExport.js
@@ -2,22 +2,26 @@ define([
     'intern!object',
     'intern/chai!assert',
     'dstore/Memory',
+    'dstore/Tree',
     'src/DataExport',
     'dgrid/OnDemandGrid',
     'dojo/_base/declare',
     'dgrid/ColumnSet',
     'dgrid/extensions/ColumnReorder',
-    'dgrid/extensions/ColumnHider'
+    'dgrid/extensions/ColumnHider',
+    'dgrid/Tree'
 ], function (
     registerSuite,
     assert,
     Memory,
+    TreeStore,
     DataExport,
     OnDemandGrid,
     declare,
     ColumnSet,
     ColumnReorder,
-    ColumnHider
+    ColumnHider,
+    Tree
 ) {
         function createNormalStore(count) {
             var rows = [];
@@ -31,6 +35,30 @@ define([
             }
 
             return new Memory({
+                data: rows
+            });
+        }
+
+        function createTreeStore(count) {
+            var rows = [];
+            for (var i = 0; i < (count || 25); i++) {
+                rows.push({
+                    id: i,
+                    field1: 'field 1 ' + i,
+                    field2: 'field 2 ' + i,
+                    field3: 'field 3 ' + i
+                });
+            }
+
+            for(i = 0; i < (count || 25); i++) {
+                rows.push({
+                    id: (count || 25) + i,
+                    field1: 'sub row field 1 ' + i,
+                    parent: i
+                });
+            }
+
+            return new declare([Memory, TreeStore])({
                 data: rows
             });
         }
@@ -172,6 +200,37 @@ define([
                         },
                         {
                             field2: 'field 2 1'
+                        }
+                    ]);
+                });
+            },
+
+            'with Tree': function () {
+                var Grid = declare([OnDemandGrid, DataExport, Tree]);
+                var grid = new Grid({
+                    collection: createTreeStore(2),
+                    columns: [
+                        { field: 'field1', label: 'field 1', renderExpando: true },
+                        { field: 'field2', label: 'field 2' }
+                    ]
+                });
+
+                return grid.exportData().then(function (data) {
+                    console.log(data);
+                    assert.deepEqual(data, [
+                        {
+                            field1: 'field 1 0',
+                            field2: 'field 2 0'
+                        },
+                        {
+                            field1: 'sub row field 1 0'
+                        },
+                        {
+                            field1: 'field 1 1',
+                            field2: 'field 2 1'
+                        },
+                        {
+                            field1: 'sub row field 1 1'
                         }
                     ]);
                 });
